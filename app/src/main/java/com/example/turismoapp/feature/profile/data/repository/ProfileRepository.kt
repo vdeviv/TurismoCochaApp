e3887289cec8a62740c0a5333546eee2862ade95
Wenba1 <balderramawendym@gmail.com>
2025-11-14 08:36:13 -0400

    Feat: Se muestra la pantalla de Profile

--- app/src/main/java/com/example/turismoapp/feature/profile/data/repository/ProfileRepository.kt
+++ app/src/main/java/com/example/turismoapp/feature/profile/data/repository/ProfileRepository.kt
@@ -3,15 +3,16 @@ package com.example.turismoapp.feature.profile.data.repository
 import com.example.turismoapp.feature.profile.domain.model.ProfileModel
 import com.example.turismoapp.feature.profile.domain.repository.IProfileRepository
 
-class ProfileRepository: IProfileRepository {
+class ProfileRepository : IProfileRepository {
+
     override fun fetchData(): Result<ProfileModel> {
         return Result.success(
             ProfileModel(
                 name = "Vivi Revollo",
                 email = "vivi.omg@gmail.com",
                 cellphone = "+777 7777",
-                pathUrl = " ",
-                summary = "No me esta dando el viewmodel help"
+                pathUrl = "https://cdn-icons-png.flaticon.com/512/149/149071.png", // URL VÁLIDA
+                summary = "No me está dando el ViewModel help"
             )
         )
     }
