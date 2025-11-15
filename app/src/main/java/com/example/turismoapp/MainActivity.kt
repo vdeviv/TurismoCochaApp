82246db8a3e4b593d0f14d19c7659e11e796537f
vdeviv <revollovivian@gmail.com>
2025-11-15 09:39:18 -0330

    base de datos - ui

--- app/src/main/java/com/example/turismoapp/MainActivity.kt
+++ app/src/main/java/com/example/turismoapp/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.turismoapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.turismoapp.feature.navigation.AppNavigation
 import com.example.turismoapp.ui.theme.TurismoAppTheme
+import com.google.firebase.FirebaseApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,12 @@ class MainActivity : ComponentActivity() {
             TurismoAppTheme(darkTheme = false, dynamicColor = false) {
                 AppNavigation()
             }
+        }
+        try {
+            FirebaseApp.initializeApp(this)
+            Log.d("Firebase", "Firebase inicializado correctamente")
+        } catch (e: Exception) {
+            Log.e("Firebase", "Error al inicializar Firebase: ${e.message}")
         }
     }
 }
