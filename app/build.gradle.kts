8994633dcb3f460600799fc18a994ebc3b3dc8d5
Wenba1 <balderramawendym@gmail.com>
2025-12-11 23:12:48 -0400

    Feat: Se agrego la version y se subio a la play store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.turismoapp.mayuandino"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
