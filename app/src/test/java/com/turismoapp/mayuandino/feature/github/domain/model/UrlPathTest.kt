a60e9f1af1cf512dc3c3cc174fc1b26686b4e137
Wenba1 <balderramawendym@gmail.com>
2025-12-18 17:29:31 -0400

    Fix: Arreglos de test de url

--- app/src/test/java/com/turismoapp/mayuandino/feature/github/domain/model/UrlPathTest.kt
+++ app/src/test/java/com/turismoapp/mayuandino/feature/github/domain/model/UrlPathTest.kt
@@ -2,10 +2,18 @@ package com.turismoapp.mayuandino.feature.github.domain.model
 
 import com.turismoapp.mayuandino.feature.profile.domain.vo.UrlPath
 import org.junit.Test
+import org.junit.Assert.assertEquals
+
 class UrlPathTest {
-    @Test(expected = Exception::class)
-    fun testUrlPath() {
-        UrlPath("myurl")
+
+    @Test(expected = IllegalArgumentException::class)
+    fun empty_url_throws_exception() {
         UrlPath("")
+    }
+
+    @Test
+    fun valid_url_is_created_successfully() {
+        val url = UrlPath("myurl")
+        assertEquals("myurl", url.value)
     }
 }
